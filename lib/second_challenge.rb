78b4242ede92a44891aed99903522b828b567c79
Adam Workman <adammworkman25@gmail.com>
2020-06-19 14:57:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,13 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {}
+      :matriarch => {}
+      :hreo => {}
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
       }}
 
   
