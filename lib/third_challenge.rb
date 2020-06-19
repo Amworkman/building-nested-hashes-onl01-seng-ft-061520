75bd7500915a1e8958502baba720fd744fc6337a
Adam Workman <adammworkman25@gmail.com>
2020-06-19 15:11:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name, "Lord Montague",
+        name: "Lord Montague",
         :age, "53"
         },
       :matriarch => {
@@ -12,7 +12,7 @@ def third_challenge
       :hero => {
         name: "Romeo",
         age: "15",
-        status:
+        status: "alive"
         },
       :hero_friends => []
    }, 
